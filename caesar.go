e20b9c81b30b454e3dd84b115069dea28bb9d028
Guilherme Eiti <guilhermemiua@gmail.com>
2019-09-24 12:30:56 -0300

    Code refactored

--- caesar.go
+++ caesar.go
@@ -6,29 +6,31 @@ import (
 )
 
 func main() {
-	encipher("!hello world", 3)
+	encoder("!hello world", 3)
 }
 
-func encipher(word string, leap int) {
-	letters := "abcdefghijklmnopqrstuvwxyz"
+func encoder(word string, leap int) {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
 	word = strings.ToLower(word)
 	var cipher string = ""
 
 	for i := 0; i < len(word); i++ {
 		var letter string = string(word[i])
-		var index int = strings.Index(letters, letter)
+		var index int = strings.Index(alphabet, letter)
 		var position int = index + leap
 
+		// Verify if it is a letter not included in alphabet
 		if index < 0 {
 			cipher = cipher + string(word[i])
 			continue
 		}
 
+		// Verify if index + leap passed the alphabet index
 		if position > 25 {
 			position = position - 25
 		}
 
-		cipher = cipher + string(letters[position])
+		cipher = cipher + string(alphabet[position])
 	}
 
 	fmt.Println(word)
